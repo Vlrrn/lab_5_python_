e973135dc62dcb2cd5168197dd2942ae690c2ad2
Vlrrn <majviasna@gmail.com>
2023-10-25 11:00:07 +0300

    5.3.1

--- 5.3.1.py
+++ 5.3.1.py
@@ -7,9 +7,17 @@ def fun(a):
     return y
 
 
+def fun2(a):
+    return a
+
+
+title_font = {"fontsize": 25, "color": "#00A693"}
+label_font = {"fontsize": 12, "color": "#075878"}
+legend_font = {"size": 10}
 arr = np.arange(-5, 12.1, 0.5)
 s = arr.size
 list_ = []
+list_m = []
 for i in arr:
     f = fun(i)
     list_.append(f)
@@ -17,16 +25,18 @@ for i in arr:
 f_x = np.array(list_).reshape(s, 1)
 print(f_x)
 m = f_x.mean()
-print("min = ", f_x.min(), "\nmax = ", f_x.max(), "\nmean = ",m, "\ncol = ", s)
+for i in arr:
+    f2 = fun2(m)
+    list_m.append(f2)
+f_m = np.array(list_m)
+print("min = ", f_x.min(), "\nmax = ", f_x.max(), "\nmean = ", m, "\ncol = ", s)
 f1 = np.sort(f_x, axis=0)
 print(f1)
-plt.plot(arr, f_x, label="f(x)")
-plt.plot([arr[0], arr[-1]], [m, m], label="mean", color="#00A693", linestyle="dashed")
-plt.title(
-    "Графики",
-    fontsize=25,
-    color="#00A693")
-plt.xlabel("Переменая x")
-plt.ylabel("Функция f(x)")
-plt.legend()
+plt.plot(arr, f_x, label="f(x)", color="#106cc2", marker="*")
+plt.plot(arr, f_m, label="mean", color="#00A693", marker="8")
+plt.title("Графики", fontdict=title_font)
+plt.xlabel("Переменая x", fontdict=label_font)
+plt.ylabel("Функция f(x)", fontdict=label_font)
+legend = plt.legend(loc="lower right", prop=legend_font)
+plt.setp(legend.get_texts(), color="#8c0a8c")
 plt.show()
